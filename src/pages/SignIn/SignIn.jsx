c8b90ec6d9f04caa6c51de2c21ccaea2eb31143c
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2023-01-24 22:04:13 -0400

    feat: creating form to Sign Up Page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,5 +1,28 @@
+import './SignIn.css'
+
 export function SignIn() {
     return (
-        <h1>Page for Sign In</h1>
+        <section className='form__container'>
+            <h2>Sign In</h2>
+            <form className='form_signIn'>
+                <fieldset className='form__fieldsButtons'>
+                    <p>Do you have an HBO Max account?</p>
+                    <div className='input__signIn'>
+                        <input type='email' name='email__address' className='input__signIn' placeholder='Email Address' />
+                        <input type='password' name='password__SignIn' className='password__SignIn' placeholder='Password' />
+                    </div>
+                    <div className='validation__signIn'>
+                        <button>SIGN IN</button>
+                        <p>Forgot password?</p>
+                    </div>
+                    <p>or</p>
+                    <div className='alternative__signIn'>
+                        <p>Do you get HBO Max Through another company such as Tv, Mobile, or internet provider?</p>
+                        <button>SIGN IN WITH A PROVIDER</button>
+                    </div>
+                </fieldset>
+            </form>
+            <p>Need Help Sign In?</p>
+        </section>
     );
 };
